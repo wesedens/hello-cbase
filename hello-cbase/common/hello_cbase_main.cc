511159891f1581627675994cfc3371efe5487f73
Wes Edens <wes.k.edens@gmail.com>
2014-02-26 00:34:36 -0500

    add a little more to the example

--- hello-cbase/common/hello_cbase_main.cc
+++ hello-cbase/common/hello_cbase_main.cc
@@ -2,21 +2,50 @@
 
 #include "base/base_switches.h"
 #include "base/command_line.h"
+#include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/version.h"
 #include "version.h"  // NOLINT
 
+
+typedef std::string StringType;
+
+
+void logging_test()
+{
+    // init the logging api
+    logging::DcheckState dcheck_state =
+        logging::ENABLE_DCHECK_FOR_NON_OFFICIAL_RELEASE_BUILDS;
+    logging::LoggingSettings setting;
+    setting.logging_dest = logging::LOG_DEFAULT;
+    setting.dcheck_state = dcheck_state;
+    logging::InitLogging(setting);
+
+    printf("\n\nLogging Example\n");
+    // show some examples of logging
+    if (VLOG_IS_ON(2)) {
+        printf("vlog is on 2\n");
+    }
+
+    LOG(INFO) << "printed in debug and release";
+    DLOG(INFO) << "only printed when compiled with debug flags";
+    VLOG(1) << "printed when you run with --v=1 or more";
+    LOG_ASSERT(1);
+    DLOG_ASSERT(1);
+}
+
+
 int main(int argc, char* argv[])
 {
     static Version cbase_version(CBASE_VERSION_STRING);
-    logging::LoggingSettings ls;
-    ls.logging_dest = logging::LOG_DEFAULT;
     CHECK(CommandLine::Init(argc, argv));
-    logging::InitLogging(ls);
-
     const CommandLine& command_line = *CommandLine::ForCurrentProcess();
-    LOG(INFO) << LASTCHANGE_STRING;
-    LOG(INFO) << cbase_version.GetString();
+    const StringType program_name = command_line.GetProgram().BaseName().value();
+    const char* version =cbase_version.GetString().c_str();
+    printf("%s: version: %s\n", program_name.c_str(), version);
+    printf("lastchange hash: %s\n", LASTCHANGE_STRING);
+
+
     std::string greeting = command_line.GetSwitchValueASCII("greeting");
     if (greeting.empty())
         greeting = "Hello";
@@ -24,21 +53,9 @@ int main(int argc, char* argv[])
     if (name.empty())
         name = "world";
 
-    printf("has ascii v=%s\n", command_line.GetSwitchValueASCII(switches::kV).c_str());
-
-
-    if (VLOG_IS_ON(2)) {
-        printf("vlog is on 2\n");
-    }
-    printf("has verbosity=%d\n", command_line.HasSwitch(switches::kV));
-    printf("verbosity=%d\n", logging::GetVlogVerbosity());
     CHECK_GT(printf("%s, %s!\n", greeting.c_str(), name.c_str()), 0);
-    LOG(INFO) << greeting << ", " << name << "!";
 
-    LOG(INFO) << "printed in release";
-    DLOG(INFO) << "only printed when compiled with debug flags";
-    VLOG(1) << "I'm printed when you run the program with --v=1 or more";
-    LOG_ASSERT(1);
-    DLOG_ASSERT(1);
+    logging_test();
+
     return 0;
 }
